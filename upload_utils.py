7d07b0b93265d445ff4972502dd6755f9af75a32
parsa <parsa@beastbrain.org>
2023-09-26 15:32:01 +0330

    added zip and upload directory

--- upload_utils.py
+++ upload_utils.py
@@ -45,6 +45,52 @@ def upload_file(local_file, base_url, directory, username, passw):
     return True
 
 
+def zipandupload_directories(
+    localdir_path, base_url, remote_directory, username, passw
+):
+    import shutil
+    import os
+
+    # list folders in local directory path
+
+    folders = [
+        f
+        for f in os.listdir(localdir_path)
+        if os.path.isdir(os.path.join(localdir_path, f))
+    ]
+    zipped = []
+    uploaded = []
+    if os.path.exists("zipped.txt"):
+        zipped = open("zipped.txt", "r").readlines()[0].split(",")
+
+    if os.path.exists("uploaded.txt"):
+        uploaded = open("uploaded.txt", "r").readlines()[0].split(",")
+
+    for folder in folders:
+        dir_name = os.path.join(localdir_path, folder)
+        output_filename = os.path.join(localdir_path, folder)
+        print(f"Zipping {folder}")
+        if folder not in zipped:
+            shutil.make_archive(output_filename, "zip", dir_name)
+            zipped.append(folder)
+            with open("zipped.txt", "w") as f:
+                f.write(",".join(zipped))
+        print(f"Uploading {folder}")
+        if folder not in uploaded:
+            upload_file(
+                local_file=f"{output_filename}.zip",
+                base_url=base_url,
+                directory=remote_directory,
+                username=username,
+                passw=passw,
+            )
+            uploaded.append(folder)
+            with open("uploaded.txt", "w") as f:
+                f.write(",".join(uploaded))
+
+    print("Done")
+
+
 # EXAMPLE USAGE
 # upload_dir(
 #     base_url="{place you want to upload}",
@@ -69,6 +115,14 @@ def upload_file(local_file, base_url, directory, username, passw):
 #     username="user",
 #     passw="pass",
 # )
+
+zipandupload(
+    localdir_path="test",
+    base_url="https://nx36834.your-storageshare.de/",
+    remote_directory="test",
+    username="",
+    passw="",
+)
 
 # via curl loop
 # count=1
